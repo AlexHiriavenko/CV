304ef633a90688a49495f8cf3f033ceef647fd66
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-07-07 19:54:28 +0300

    feat: media queries

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const btnCode = document.querySelector(".code__btn");
 const btnClose = document.querySelector(".close");
+const btnBurger = document.querySelector(".burger-btn");
+const linkCert = document.querySelector(".cert");
+const headerLinks = document.querySelectorAll(".nav-link");
 
 btnCode.addEventListener("click", onBtnCodeClick);
 
@@ -14,3 +17,14 @@ btnClose.addEventListener("click", () => {
     const codePreview = document.getElementById("code");
     codePreview.classList.remove("code-onClick");
 });
+
+btnBurger.addEventListener("click", handleBurger);
+
+function handleBurger() {
+    const headerMenu = document.querySelector(".nav-list");
+    headerMenu.matches(".nav-list--active")
+        ? headerMenu.classList.remove("nav-list--active")
+        : headerMenu.classList.add("nav-list--active");
+}
+
+headerLinks.forEach((link) => link.addEventListener("click", handleBurger));
